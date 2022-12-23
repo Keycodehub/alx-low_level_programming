be3ee0f60004e01d85a1443e864c47e60d3f18d6
Keycodehub <ogunyooyeolayinka@gmail.com>
2022-12-23 02:17:56 -0800

    1 strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -21,7 +21,7 @@ char *_strncat(char *dest, char *src, int n)
 	}
 	for (j = 0; j < n && src[j] != '\0'; j++, length++)
 	{
-		dest[length] = src[j];   
+		dest[length] = src[j];
 	}
 
 	dest[length] = '\0';
